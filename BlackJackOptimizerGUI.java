ca198e60b3be6686351af8a6decb57d889b18a43
FLee <lfredl109@gmail.com>
2015-06-01 09:57:39 -0700

    Just having fun

--- BlackJackOptimizerGUI.java
+++ BlackJackOptimizerGUI.java
@@ -1,35 +1,78 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
-public class Test implements ActionListener {
-    JButton button;
+public class BlackJackOptimizerGUI implements ActionListener {
+    JFrame frame;
+    public static void main(String[] args) {
+        BlackJackOptimizerGUI gui = new BlackJackOptimizerGUI();
+        gui.go();
+    }
 
     public void go() {
-        JFrame frame = new JFrame();
-        button = new JButton("Click to Start BlackJackOptimizer");
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //this says "Add me to your list of listeners." The argument
-        //must be an object from a class that implements ActionListener in this case
+        JButton button = new JButton("Change colors");
         button.addActionListener(this);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(button);
-        frame.setSize(300,300);
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.setSize(300, 300);
         frame.setVisible(true);
     }
 
-    @Override
     public void actionPerformed(ActionEvent event) {
-        button.setText("Welcome to BlackJackOptimizer.");
+        frame.repaint();
     }
 
-    public static void main(String[] args) {
-        Test gui = new Test();
-        gui.go();
+    class MyDrawPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            // g.setColor(Color.BLUE);
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+            int red = (int) (Math.random() * 255);
+            int green = (int) (Math.random() * 255);
+            int blue = (int) (Math.random() * 255);
+
+            Color randomColor = new Color(red, green, blue);
+            g.setColor(randomColor);
+            g.fillOval(70, 70, 100, 100);
+        }
     }
 }
 
-// public class Test extends JPanel {
+// public class BlackJackOptimizerGUI implements ActionListener {
+//     JButton button;
+
+//     public void go() {
+//         JFrame frame = new JFrame();
+//         button = new JButton("Click to Start BlackJackOptimizer");
+
+//         //this says "Add me to your list of listeners." The argument
+//         //must be an object from a class that implements ActionListener in this case
+//         button.addActionListener(this);
+
+//         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//         frame.getContentPane().add(BorderLayout.CENTER, button);
+//         frame.setSize(300,300);
+//         frame.setVisible(true);
+//     }
+
+//     @Override
+//     public void actionPerformed(ActionEvent event) {
+//         button.setText("Welcome to BlackJackOptimizer.");
+//     }
+
+//     public static void main(String[] args) {
+//         BlackJackOptimizerGUI gui = new BlackJackOptimizerGUI();
+//         gui.go();
+//     }
+// }
+
+// public class BlackJackOptimizerGUI extends JPanel {
 //     public void paintComponent(Graphics g) {
 //     	g.setColor(Color.orange);
 //     	g.fillRect(20,50,100,100);
